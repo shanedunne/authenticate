b2703aa92dcac6e80e80b9c92f0e035d68b4c37c
shanedunne <shanejjdunne@hotmail.com>
2022-06-09 09:39:12 +0200

    add some config to react

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from "styled-components";
+import { Colors } from "./assets/Theme";
+
+const AppEl = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+`;
+
+const Title = styled.h1`
+  color: ${Colors.Primary};
+  border: 2px solid ${Colors.Primary};
+  padding: 0.5rem 1rem;
+  border-radius: 20px;
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppEl>
+      <Title>Go ahead and start!</Title>
+    </AppEl>
   );
 }
 
