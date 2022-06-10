78bb9f153c5e2fcf7736f8f683143ae4122f4c6c
shanedunne <shanejjdunne@hotmail.com>
2022-06-10 19:09:01 +0200

    add sample nft contract for hardhat testing

--- contracts/NFT.sol
+++ contracts/NFT.sol
@@ -1,29 +1,32 @@
-// SPDX-License-Identifier: MIT OR Apache-2.0
-pragma solidity ^0.8.3;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
 
-import "@openzeppelin/contracts/utils/Counters.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
-import "hardhat/console.sol";
+contract BasicNft is ERC721 {
+    string public constant TOKEN_URI =
 
-contract NFT is ERC721URIStorage {
-    using Counters for Counters.Counter;
-    Counters.Counter private _tokenIds;
-    address contractAddress;
+"ipfs://bafybeig37ioir76s7mg5oobetncojcm3c3hxasyd4rvid4jqhy4gkaheg4/?filename=0-PUG.json";
+    uint256 private s_tokenCounter;
 
-    constructor(address marketplaceAddress) ERC721("Eat The Blocks NFTs", "ETBNFT") {
-        contractAddress = marketplaceAddress;
+    event DogMinted(uint256 indexed tokenId);
+
+    constructor() ERC721("Dogie", "DOG") {
+        s_tokenCounter = 0;
     }
 
-    function createToken(string memory tokenURI) public returns (uint) {
-        _tokenIds.increment();
-        uint256 newItemId = _tokenIds.current();
+    function mintNft() public {
+        _safeMint(msg.sender, s_tokenCounter);
+        emit DogMinted(s_tokenCounter);
+        s_tokenCounter = s_tokenCounter + 1;
+    }
 
-        _mint(msg.sender, newItemId);
-        _setTokenURI(newItemId, tokenURI);
-        setApprovalForAll(contractAddress, true);
-        return newItemId;
+    function tokenURI(uint256 tokenId) public view override returns (string memory) {
+        require(_exists(tokenId), "ERC721Metadata: URI query for nonexistent token");
+        return TOKEN_URI;
+    }
+
+    function getTokenCounter() public view returns (uint256) {
+        return s_tokenCounter;
     }
 }
