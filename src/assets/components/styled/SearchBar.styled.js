2dcfebd0cc3fdd224aca1548dabb7fd5f43af1da
shanedunne <shanejjdunne@hotmail.com>
2022-06-10 08:37:02 +0200

    style search bar

--- src/assets/components/styled/SearchBar.styled.js
+++ src/assets/components/styled/SearchBar.styled.js
@@ -3,13 +3,35 @@ import {RiSearchLine} from "react-icons/ri";
 import { Colors } from "../../Theme";
 
 
-const SearchBarEl = styled.div``;
-const Input = styled.input``;
+const SearchBarEl = styled.div`
+    background-color: ${Colors.CardBackground};
+    display: flex;
+    align-items: center;
+    padding: 1rem;
+    border-radius: 12px;
+    margin: 0 1rem 2rem 1rem;
+
+    & > svg {
+        font-size: 1.5rem;
+        color: ${Colors.Gray};
+    }
+`;
+const Input = styled.input`
+    font-size: 1rem;
+    border: none;
+    flex: 1;
+
+    ::focus {
+        outline: none;
+    }
+
+`;
 
 export default function SearchBar() {
     return (
         <SearchBarEl>
             <Input placeholder="Search the marketplace"/>
+            <RiSearchLine />
 
         </SearchBarEl>
     )
